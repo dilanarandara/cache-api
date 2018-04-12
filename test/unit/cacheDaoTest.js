2c4d0b043b785a7f25933c9d25ce941a710b87f1
Dilan Arandara <dilan.arandara@syscolabs.com>
2018-04-12 20:55:46 +0530

    Add dummy unit test files.

---
+++ test/unit/cacheDaoTest.js
@@ -0,0 +1,12 @@
+const should = require('should');
+
+const cacheService = require('../../app/services/CacheService');
+
+// A dummy test.
+describe('Unit: CacheDao Testing', () => {
+    describe('getByKeyOrInsert', () => {
+        it('Not existing key return valid object', (done) => {
+            done();
+        });
+    });
+});
